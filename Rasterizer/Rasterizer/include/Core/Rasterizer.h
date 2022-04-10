d6ccd006876fd0dc997a60e8dd1f8547636c7d90
maxbrun <maxbrundev@gmail.com>
2022-04-10 22:57:53 +0200

    Adding DrawLine method

--- Rasterizer/Rasterizer/include/Core/Rasterizer.h
+++ Rasterizer/Rasterizer/include/Core/Rasterizer.h
@@ -12,18 +12,19 @@ namespace Core
 {
 	class Rasterizer
 	{
-		public:
+	public:
 		Rasterizer(Renderer& p_renderer, uint16_t p_textureBufferWidth, uint16_t p_textureBufferHeight);
 		~Rasterizer() = default;
 
-
 		void SetScissorRect(int p_minX, int p_minY, int p_maxX, int p_maxY);
 
-		Uint8 InterpolateColors(Uint8 p_color0, Uint8 p_color1, Uint8 p_color2, Geometry::Edge p_edge0, Geometry::Edge p_edge1, Geometry::Edge p_edge2, float p_area, float p_positionX, float p_positionY);
-
-		void DrawTriangle(const Geometry::Vertex& p_vertex0, const Geometry::Vertex& p_vertex1, const Geometry::Vertex& p_vertex2);
+		void DrawLine(const Geometry::Vertex& p_vertex0, const Geometry::Vertex& p_vertex1) const;
+		void DrawTriangle(const Geometry::Vertex& p_vertex0, const Geometry::Vertex& p_vertex1, const Geometry::Vertex& p_vertex2) const;
 
 		Buffers::TextureBuffer& GetTextureBuffer();
+
+	private:
+		Uint8 InterpolateColors(Uint8 p_color0, Uint8 p_color1, Uint8 p_color2, Geometry::Edge p_edge0, Geometry::Edge p_edge1, Geometry::Edge p_edge2, float p_area, float p_positionX, float p_positionY) const;
 
 	private:
 		Buffers::TextureBuffer m_textureBuffer;
--- Rasterizer/Rasterizer/src/Core/Rasterizer.cpp
+++ Rasterizer/Rasterizer/src/Core/Rasterizer.cpp
@@ -15,7 +15,7 @@ void Core::Rasterizer::SetScissorRect(int p_minX, int p_minY, int p_maxX, int p_
 	m_maxY = p_maxY;
 }
 
-Uint8 Core::Rasterizer::InterpolateColors(Uint8 p_color0, Uint8 p_color1, Uint8 p_color2, Geometry::Edge p_edge0, Geometry::Edge p_edge1, Geometry::Edge p_edge2, float p_area, float p_positionX, float p_positionY)
+Uint8 Core::Rasterizer::InterpolateColors(Uint8 p_color0, Uint8 p_color1, Uint8 p_color2, Geometry::Edge p_edge0, Geometry::Edge p_edge1, Geometry::Edge p_edge2, float p_area, float p_positionX, float p_positionY) const
 {
 	float factor = 1.0f / (2.0f * p_area);
 
@@ -26,7 +26,82 @@ Uint8 Core::Rasterizer::InterpolateColors(Uint8 p_color0, Uint8 p_color1, Uint8 
 	return a * p_positionX + b * p_positionY + c;
 }
 
-void Core::Rasterizer::DrawTriangle(const Geometry::Vertex& p_vertex0, const Geometry::Vertex& p_vertex1, const Geometry::Vertex& p_vertex2)
+void Core::Rasterizer::DrawLine(const Geometry::Vertex& p_vertex0, const Geometry::Vertex& p_vertex1) const
+{
+	const float xdiff = (p_vertex1.x - p_vertex0.x);
+	const float ydiff = (p_vertex1.y - p_vertex0.y);
+
+	if (xdiff == 0.0f && ydiff == 0.0f)
+	{
+		m_textureBuffer.SetPixel(p_vertex0.x, p_vertex0.y, p_vertex0.color);
+		return;
+	}
+
+	if (fabs(xdiff) > fabs(ydiff))
+	{
+		uint16_t xmin;
+		uint16_t xmax;
+
+		if (p_vertex0.x < p_vertex1.x) 
+		{
+			xmin = p_vertex0.x;
+			xmax = p_vertex1.x;
+		}
+		else
+		{
+			xmin = p_vertex1.x;
+			xmax = p_vertex0.x;
+		}
+
+		const uint16_t slope = ydiff / xdiff;
+
+		for (uint16_t x = xmin; x <= xmax; x++)
+		{
+			const uint16_t y = p_vertex0.y + ((x - p_vertex0.x) * slope);
+
+			const Uint8 red   = p_vertex0.color.r + (p_vertex1.color.r - p_vertex0.color.r) * ((x - p_vertex0.x) / xdiff);
+			const Uint8 green = p_vertex0.color.g + (p_vertex1.color.g - p_vertex0.color.g) * ((x - p_vertex0.x) / xdiff);
+			const Uint8 blue  = p_vertex0.color.b + (p_vertex1.color.b - p_vertex0.color.b) * ((x - p_vertex0.x) / xdiff);
+
+			Data::Color color(red, green, blue);
+
+			m_textureBuffer.SetPixel(x, y, color);
+		}
+	}
+	else
+	{
+		uint16_t ymin;
+		uint16_t ymax;
+
+		if (p_vertex0.y < p_vertex1.y)
+		{
+			ymin = p_vertex0.y;
+			ymax = p_vertex1.y;
+		}
+		else
+		{
+			ymin = p_vertex1.y;
+			ymax = p_vertex0.y;
+		}
+
+		const uint16_t slope = xdiff / ydiff;
+
+		for (uint16_t y = ymin; y <= ymax; y++)
+		{
+			const uint16_t x = p_vertex0.x + ((y - p_vertex0.y) * slope);
+
+			const Uint8 red   = p_vertex0.color.r + (p_vertex1.color.r - p_vertex0.color.r) * ((y - p_vertex0.y) / ydiff);
+			const Uint8 green = p_vertex0.color.g + (p_vertex1.color.g - p_vertex0.color.g) * ((y - p_vertex0.y) / ydiff);
+			const Uint8 blue  = p_vertex0.color.b + (p_vertex1.color.b - p_vertex0.color.b) * ((y - p_vertex0.y) / ydiff);
+
+			Data::Color color(red, green, blue);
+
+			m_textureBuffer.SetPixel(x, y, color);
+		}
+	}
+}
+
+void Core::Rasterizer::DrawTriangle(const Geometry::Vertex& p_vertex0, const Geometry::Vertex& p_vertex1, const Geometry::Vertex& p_vertex2) const
 {
 	uint16_t minX = std::min(std::min(p_vertex0.x, p_vertex1.x), p_vertex2.x);
 	uint16_t maxX = std::max(std::max(p_vertex0.x, p_vertex1.x), p_vertex2.x);
