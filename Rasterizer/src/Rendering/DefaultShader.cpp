a7cb8df286d806df51804ed1e36b515657fc0d60
Max Brun <maxbrundev@gmail.com>
2025-02-18 23:36:47 +0100

    [Shader Rework] Implementing Shader Type Traits

--- Rasterizer/src/Rendering/DefaultShader.cpp
+++ Rasterizer/src/Rendering/DefaultShader.cpp
@@ -2,26 +2,26 @@
 
 glm::vec4 Rendering::DefaultShader::VertexPass(const Geometry::Vertex& p_vertex)
 {
-	const glm::mat4 u_Model      = GetUniformAsMat4("u_Model");
-	const glm::mat4 u_View       = GetUniformAsMat4("u_View");
-	const glm::mat4 u_Projection = GetUniformAsMat4("u_Projection");
+	const glm::mat4 u_Model      = GetUniformAs<glm::mat4>("u_Model");
+	const glm::mat4 u_View       = GetUniformAs<glm::mat4>("u_View");
+	const glm::mat4 u_Projection = GetUniformAs<glm::mat4>("u_Projection");
 
 	glm::vec3 fragPos = glm::vec3(u_Model * glm::vec4(p_vertex.position, 1.0));
-	SetFlatVec3("v_FragPos", fragPos);
+	SetFlat<glm::vec3>("v_FragPos", fragPos);
 
 	glm::vec3 normal = glm::mat3(glm::transpose(glm::inverse(u_Model))) * p_vertex.normal;
-	SetVaryingVec3("v_Normal", normal);
+	SetVarying<glm::vec3>("v_Normal", normal);
 
 	glm::vec2 texCoords = p_vertex.textCoords;
-	SetVaryingVec2("v_TextCoords", texCoords);
+	SetVarying<glm::vec2>("v_TextCoords", texCoords);
 
 	return  u_Projection * u_View * glm::vec4(fragPos, 1.0);
 }
 
 Data::Color Rendering::DefaultShader::FragmentPass()
 {
-	glm::vec3 normal     = glm::normalize(GetVaryingAsVec3("v_Normal"));
-	glm::vec2 textCoords = GetVaryingAsVec2("v_TextCoords");
+	glm::vec3 normal     = glm::normalize(GetVaryingAs<glm::vec3>("v_Normal"));
+	glm::vec2 textCoords = GetVaryingAs<glm::vec2>("v_TextCoords");
 
 	//return glm::vec4(((normal * 0.5f) + 0.5f), 1.0f);
 
